a5443349a1d1be793dd4c4c378dcfc374aeb2aea
Afonso Vasconcelos <afonso.fv@hotmail.com>
2018-11-15 16:32:22 +0000

    Person Executes finished

--- sth/app/person/DoSearchPerson.java
+++ sth/app/person/DoSearchPerson.java
@@ -2,7 +2,9 @@ package sth.app.person;
 
 import pt.tecnico.po.ui.Command;
 import pt.tecnico.po.ui.Input;
+import pt.tecnico.po.ui.Display;
 import sth.core.SchoolManager;
+import sth.core.Person;
 
 //FIXME import other classes if needed
 
@@ -10,8 +12,8 @@ import sth.core.SchoolManager;
  * 4.2.4. Search person.
  */
 public class DoSearchPerson extends Command<SchoolManager> {
-
   //FIXME add input fields if needed
+  private Input<String> _personNameInput;
   
   /**
    * @param receiver
@@ -19,12 +21,18 @@ public class DoSearchPerson extends Command<SchoolManager> {
   public DoSearchPerson(SchoolManager receiver) {
     super(Label.SEARCH_PERSON, receiver);
     //FIXME initialize input fields if needed
+    _personNameInput = _form.addStringInput(Message.requestPersonName());
   }
 
   /** @see pt.tecnico.po.ui.Command#execute() */
   @Override
   public final void execute() {
     //FIXME implement command
-  }
+    _form.parse();
+    String personName = _personNameInput.value();
+    for (Person p: _receiver.getAllUsers(personName))
+      _display.addLine(p.toString());
 
+    _display.display();
+  }
 }
